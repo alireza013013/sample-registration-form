da3766d457474961651b23da406bd507df5f2f45
alireza013013 <alireza.abdollahi19998@gmail.com>
2024-05-06 15:45:20 +0330

    comment delete ,name app change

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,10 @@
 <template>
     <Wizard />
-    <!-- <Description /> -->
 </template>
 
 
 <script setup>
 import Wizard from '@/components/Wizard.vue';
-import Description from '@/components/Description.vue';
 </script>
 
 
